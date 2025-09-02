51a6e7cd7588bd890d13f1b1d3ec32b957946332
support1122 <support@flashfirejobs.com>
2025-09-02 13:30:17 +0530

    Update JobForm.tsx

--- src/components/JobForm.tsx
+++ src/components/JobForm.tsx
@@ -169,7 +169,8 @@ const JobForm: React.FC<JobFormProps> = ({ job, onCancel, onSuccess, setUserJobs
     if (!isEditMode) {
       const optimisticId = Date.now().toString();
       // const nowIN = new Date().toLocaleString("en-US", { hour12: true, timeZone: "Asia/Kolkata" });
-      const nowIN = new Date().toISOString();
+      // const nowIN = new Date().toISOString();
+      const nowIN = new Date().toLocaleString("en-IN", { timeZone: "Asia/Kolkata", });
 
       const optimisticJob = {
         jobID: optimisticId,
